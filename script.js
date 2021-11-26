de11ee245afc4dce8cf02c2241d3926d64e1a056
sooraj71216 <soorajsuresh71216@gmail.com>
2021-11-25 20:20:06 -0800

    calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+let displayValue=(num)=>result.value+=num
+// function displayValue(num){
+//     let text_box=document.querySelector("#result")
+//     text_box.value+=num
+// }
+
+
+
+let evaluateExpression=()=>result.value=eval(result.value)
+// function evaluateExpression(){
+//     let text_box=document.querySelector("#result")
+//     text_box.value=eval(text_box.value)
+
+// }
+
+
+let clearScreen=()=>result.value=""
+// function clearScreen(){
+//     document.querySelector("#result").value=""
+// }
+
+let backSpace=()=>result.value=result.value.slice(0,-1)
+// function backSpace(){
+//     let text_box=document.querySelector("#result")
+//     text_box.value=text_box.value.slice(0,-1)
+// }
